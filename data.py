456483d991875c9fd5dc4ee5fb577199194e2089
theo-desktop <theo.farrell99@outlook.com>
2025-10-13 17:29:04 +0000

    update main branch #1 * catch up with submission branch * change dataset loading - now we generate rather than load saved, but seed is fixed so should be same each time * fix train.ipynb, not done interp.ipynb really yet * update model naming

--- data.py
+++ data.py
@@ -1,35 +1,32 @@
-# %%
 import torch
 from torch.utils.data import TensorDataset
-
-import os
 import itertools
-
-# %%
-LIST_LEN = 2 # [d1, d2]
-SEQ_LEN = LIST_LEN * 2 + 1 # [d1, d2, SEP, o1, o2]
-TRAIN_DUPES_ONLY = True # whether to add duplicates to training set only (for dupes dataset)
-NO_DUPES = False # whether to use the no-dupes test dataset (i.e. d1 != d2)
-
-N_DIGITS = 100
-DIGITS = list(range(N_DIGITS)) # 100 digits from 0 to 99
-PAD = N_DIGITS # special padding token
-SEP = N_DIGITS + 1 # special seperator token for the model to think about the input (+1 to avoid confusion with the last digit)
-
-TRAIN_SPLIT = 0.8 # 80% train, 20% test
 
 DEV = (
     "cuda"
     if torch.cuda.is_available()
     else ("mps" if torch.backends.mps.is_available() else "cpu")
 )
-device = DEV
-torch.manual_seed(0)
+torch.manual_seed(0) # for reproducibility
 
-# %%
-def get_dataset():
+def get_dataset(
+    list_len=2, # [d1, d2]
+    n_digits=100,
+    train_split=0.8, # 80% train, 20% test
+    train_dupes_only=False, # whether to remove duplicates (where d1 == d2) from the validation set
+    no_dupes=False, # whether to use only non-duplicates (i.e. all d1 != d2)
+    mask_tok=None, # special masking token for o1 and o2
+    sep_tok=None, # special seperator token for the model to think about the input 
+):
+    seq_len = list_len * 2 + 1 # [d1, d2, SEP, o1, o2]
+    digits = list(range(n_digits)) # 100 digits from 0 to 99
+    if mask_tok is None:
+        mask_tok = n_digits 
+    if sep_tok is None:
+        sep_tok = n_digits + 1 
+
     # Create all possible combinations of digits
-    all_data = list(itertools.product(DIGITS, repeat=LIST_LEN))
+    all_data = list(itertools.product(digits, repeat=list_len))
     all_data = torch.tensor(all_data, dtype=torch.int64)
 
     # Split into dupes (d1 == d2) and non-dupes
@@ -39,14 +36,14 @@ def get_dataset():
 
     def build_inputs_targets(data_tensor: torch.Tensor):
         n = len(data_tensor)
-        targets = torch.full((n, SEQ_LEN), SEP, dtype=torch.int64)
-        targets[:, :LIST_LEN] = data_tensor
-        targets[:, LIST_LEN + 1 :] = data_tensor
+        targets = torch.full((n, seq_len), sep_tok, dtype=torch.int64)
+        targets[:, :list_len] = data_tensor
+        targets[:, list_len + 1 :] = data_tensor
         inputs = targets.clone()
-        inputs[:, LIST_LEN + 1 :] = PAD
+        inputs[:, list_len + 1 :] = mask_tok
         return inputs, targets
 
-    if NO_DUPES:
+    if no_dupes:
         # Use only non-duplicates for both train and val
         all_inputs, all_targets = build_inputs_targets(non_dupes)
         n_data = len(non_dupes)
@@ -57,14 +54,14 @@ def get_dataset():
         all_targets = all_targets[perm]
 
         # Split into train/val
-        split = int(TRAIN_SPLIT * n_data)
+        split = int(train_split * n_data)
         train_inputs = all_inputs[:split]
         train_targets = all_targets[:split]
         val_inputs = all_inputs[split:]
         val_targets = all_targets[split:]
 
     else: # if allowed dupes
-        if TRAIN_DUPES_ONLY:
+        if train_dupes_only:
             # Split non-dupes into train/val, then add dupes only to train and reshuffle
             nd_inputs, nd_targets = build_inputs_targets(non_dupes)
             n_nd = len(non_dupes)
@@ -75,7 +72,7 @@ def get_dataset():
             nd_targets = nd_targets[perm]
 
             # Split non-dupes into train/val
-            split = int(TRAIN_SPLIT * n_nd)
+            split = int(train_split * n_nd)
             train_inputs = nd_inputs[:split]
             train_targets = nd_targets[:split]
             val_inputs = nd_inputs[split:]
@@ -101,7 +98,7 @@ def get_dataset():
             all_targets = all_targets[perm]
 
             # Split into train/val
-            split = int(TRAIN_SPLIT * n_data)
+            split = int(train_split * n_data)
             train_inputs = all_inputs[:split]
             train_targets = all_targets[:split]
             val_inputs = all_inputs[split:]
@@ -111,31 +108,3 @@ def get_dataset():
     val_ds = TensorDataset(val_inputs, val_targets)
 
     return train_ds, val_ds
-
-# %%
-if __name__ == "__main__":
-    dupes_suffix = 'nodupes' if NO_DUPES else 'dupes'
-    if TRAIN_DUPES_ONLY and not NO_DUPES:
-        dupes_suffix += '_traindupesonly'
-
-    DATASET_NAME = f"listlen{LIST_LEN}_digits{N_DIGITS}_{dupes_suffix}"
-    DATASET_PATH = f"data/{DATASET_NAME}.pt"
-
-    if os.path.exists(DATASET_PATH):
-        print(f"{DATASET_PATH} already exists. Please delete it or change the dataset name.")
-        raise FileExistsError()
-
-    train_ds, val_ds = get_dataset()
-
-    torch.save({
-        'train': train_ds,
-        'val': val_ds
-    }, DATASET_PATH)
-
-    print(f"Dataset saved to {DATASET_PATH}")
-    print("Train dataset size:", len(train_ds))
-    print("Validation dataset size:", len(val_ds))
-    print("Input example:", train_ds[0][0])
-    print("Target example:", train_ds[0][1])
-
-
